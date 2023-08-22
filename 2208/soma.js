90dad6973255f14bafe11b67114a673a2a355e61
AnnaJuliaOliveira <142810674+AnnaJuliaOliveira@users.noreply.github.com>
2023-08-22 08:48:10 -0300

    arquivo js1.js

--- 2208/soma.js
+++ 2208/soma.js
@@ -2,3 +2,12 @@
 let soma= 5+3;
 console.log(soma);
 //Resultado:8
+
+let soma2 = 5+"3";
+console.log(soma);
+//resultado:53
+
+//Multiplicação
+let mult = 5* "3";
+console.log(mult);
+//Resultado:15
