c2a5767bf3d831694b23be36811ab4d20cbc7dac
AnnaJuliaOliveira <142810674+AnnaJuliaOliveira@users.noreply.github.com>
2023-08-22 09:27:39 -0300

    divisão.js4

---
+++ 2208/divisão.js
@@ -0,0 +1,16 @@
+//Divisão:
+let divisao=10/2;
+console.log(divisao);
+//Resultado:5
+
+//Divisao de numeros decimais:
+let decimal1=5.5;
+let decimal2=2;
+ divisao=decimal1/decimal2;
+ console.log(divisao);
+ //Resultado:2.75
+
+ //Divisão por zero:
+ let resultado=10/0;
+ console.log(resultado);
+ //Resultado:Infinity
