4711afa6a249c93e06065dd3d40239d978029e01
AnnaJuliaOliveira <142810674+AnnaJuliaOliveira@users.noreply.github.com>
2023-08-22 09:15:36 -0300

    subtração.js3

---
+++ 2208/subtração.js
@@ -0,0 +1,16 @@
+//Subtração de dois números:
+let subtracao = 8-3;
+console.log(subtracao);
+//resultado:5
+
+//Subtraçao de números decimais:
+let decimal1=5.7;
+let decimal2=2.3;
+ subtracao= decimal1-decimal2;
+console.log(subtracao);
+
+//subtração de variáveis:
+let num1=10;
+let num2=3;
+let resultado=num1-num2;
+console.log(resultado);
