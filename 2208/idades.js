7bf4842505c3fb95a6d441b2c951e8ebd9635a22
AnnaJuliaOliveira <142810674+AnnaJuliaOliveira@users.noreply.github.com>
2023-08-22 11:48:26 -0300

    soma das idades

---
+++ 2208/idades.js
@@ -0,0 +1,8 @@
+let idpai=43;
+let idmae=33;
+let idfilho=18;
+let idfinal=idpai+idfilho+idmae;
+console.log("Idade do pai:",idpai);
+console.log("idade da mãe:",idmae);
+console.log("idade do filho:", idfilho);
+console.log("a idade de todas as pessoas da casa é de:",idfinal);
