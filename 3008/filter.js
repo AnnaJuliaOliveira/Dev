a83b898abd7f124e0fe1c99b93731786dd745849
AnnaJuliaOliveira <142810674+AnnaJuliaOliveira@users.noreply.github.com>
2023-08-30 11:11:25 -0300

    reduce em javascript

---
+++ 3008/filter.js
@@ -0,0 +1,7 @@
+let idade = [18,25,12,35,40];
+let adulto = idade.filter(function(age){
+return age >= 18;
+
+
+});
+console.log(adulto);
---
+++ 3008/reduce.js
@@ -0,0 +1,10 @@
+let numeros =[1,2,3,4,5];
+let soma = numeros.reduce(function(total, somanumeros){
+    return total + somanumeros;
+}, 0);
+console.log(soma);
+
+
+
+
+
