024025ac0b0963c7289a8be5894cfb74a53c686b
AnnaJuliaOliveira <142810674+AnnaJuliaOliveira@users.noreply.github.com>
2023-08-30 11:47:58 -0300

    formatação

---
+++ 3008/formatação.js
@@ -0,0 +1,6 @@
+let nome = 'João';
+let sobrenome = 'Silva';
+let nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+let mensagem = `Olá, ${nome} ${sobrenome}!`;
+console.log(mensagem);
