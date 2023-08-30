9b100ec989a60bac246769eff8b0ba7bc370a4f0
AnnaJuliaOliveira <142810674+AnnaJuliaOliveira@users.noreply.github.com>
2023-08-30 09:14:06 -0300

    arrays teste

---
+++ 3008/arrays.js
@@ -0,0 +1,10 @@
+
+let numeros = [1, 2, 3, 4, 5];
+
+var vetor1 = [1, 2, 3];
+var vetor2 = [4, 5, 6];
+var vetor3 = [7, 8, 9];
+var resultado = vetor1.concat(vetor2, vetor3);
+var resultado1 = vetor1, vetor2, vetor3;
+console.log(resultado);
+
