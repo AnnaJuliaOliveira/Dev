4e29838794cacd6496cb48eaadff183e37757499
AnnaJuliaOliveira <142810674+AnnaJuliaOliveira@users.noreply.github.com>
2023-08-30 09:25:21 -0300

    cores em js

---
+++ 3008/cores.js
@@ -0,0 +1,7 @@
+var cores1 = ['rosa','amarelo', 'azul','preto'];
+var cores2 = ['laranja','vermleho','branco', 'rosa'];
+
+console.log('Conteudo do vetor 1:', cores1.join(','));
+console.log('Comnteudo do vetor 2:' , cores2.join(','));
+console.log('Segunda posiçao do vetor 1:', cores1[1]);
+console.log('Terceira posição do vetor 2:', cores2[2]);
